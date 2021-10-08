930d197a66d9cdbecba9e3320130af9dbc586ad3
DeniseCruz <denisecz@gmail.com>
2021-10-08 15:16:05 -0300

    update user

--- src/main/java/com/example/workshopmongo/resources/UserResource.java
+++ src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -81,4 +81,15 @@ public class UserResource {
 		return ResponseEntity.noContent().build(); ///204 resposa em nada
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+			
+	}
+	
 }
