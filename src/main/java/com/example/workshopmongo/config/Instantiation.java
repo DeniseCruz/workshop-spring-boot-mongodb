927aa2d2302beccb6aabb31f112d71da0426a4c5
DeniseCruz <denisecz@gmail.com>
2021-10-13 10:56:08 -0300

    Author com DTO ao inves de user direto

--- src/main/java/com/example/workshopmongo/config/Instantiation.java
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.workshopmongo.domain.Post;
 import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.dto.AuthorDTO;
 import com.example.workshopmongo.repository.PostRepository;
 import com.example.workshopmongo.repository.UserRepository;
 
@@ -37,12 +38,15 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		////instanciar automaticamente
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar São Paulo. Abraços !", maria );
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje !", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		////instanciar automaticamente
+		///Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar São Paulo. Abraços !", maria );
+		///Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje !", maria);
+		////ANTES sem DTO
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar São Paulo. Abraços !", new AuthorDTO(maria) );
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje !", new AuthorDTO(maria));
+				
 		
 		postRepository.saveAll(Arrays.asList(post1,post2)) ;
 		
