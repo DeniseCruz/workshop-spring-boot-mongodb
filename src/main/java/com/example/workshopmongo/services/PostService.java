1d423417f4b215f9fa22df417999ce690adb4f8c
DeniseCruz <denisecz@gmail.com>
2021-10-14 10:51:01 -0300

    Obter um post por id

---
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -0,0 +1,34 @@
+package com.example.workshopmongo.services;
+
+import java.util.Optional;
+
+///import javax.management.AttributeNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongo.domain.Post;
+
+import com.example.workshopmongo.repository.PostRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+   
+		
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+					
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+			
+		}
+		///return user.get();
+		return user.orElseThrow(() -> new ObjectNotFoundException(id)) ;
+		
+	}
+	
+}
